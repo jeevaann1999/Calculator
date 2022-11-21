ad015b77026eeeecd865d2b4e2de99c5aa7ea891
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-21 11:50:41 +0530

    Added switch

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static double sum(double i, double j)
@@ -25,6 +27,45 @@ public class Main
     }
     public static void main(String[] args)
     {
-        System.out.println("Create a Calculator");
+        int flag = 0;
+        while(flag==0) {
+            System.out.println("Ready to Calculate");
+            System.out.println("1.Add\n" +
+                    "2.Subtract\n" +
+                    "3.Multiply\n" +
+                    "4.Divide\n" +
+                    "5.Exit\n");
+
+            Scanner input=new Scanner(System.in);
+            System.out.println("Enter first no");
+            double a = input.nextDouble();
+            System.out.println("Enter second no");
+            double b = input.nextDouble();
+            System.out.println("Select which calculation to do by pressing 1,2,3,4 or 5");
+            int option = input.nextInt();
+            switch (option) {
+                case 1:
+                    double add = sum(a, b);
+                    System.out.println(add);
+                    break;
+                case 2:
+                    double sub = subtract(a,b);
+                    System.out.println(sub);
+                    break;
+                case 3:
+                    double mul = multiply(a,b);
+                    System.out.println(mul);
+                    break;
+                case 4:
+                    double div = div(a,b);
+                    System.out.println(div);
+                    break;
+                case 5:
+                    System.out.println("exit");
+                    break;
+                default:
+                    System.out.println("Invalid Options");
+            }
+        }
     }
 }
